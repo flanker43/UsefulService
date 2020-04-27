359a93f95419f3dfaaba4c464b66cf5ff65a663c
flanker43 <Flanker0dayz>
2020-04-27 15:03:22 +0300

    Okhttp + ObjectMapper

--- src/main/java/Controllers/Rest.java
+++ src/main/java/Controllers/Rest.java
@@ -1,5 +1,9 @@
 package Controllers;
 
+import Dto.CountryDto;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -8,12 +12,19 @@ import java.io.IOException;
 
 public class Rest {
 
-  private static OkHttpClient client = new OkHttpClient();
-  static String response1;
+    private static OkHttpClient client = new OkHttpClient();
+    //  static String response1;
+    static String country1;
+    static long confirmed1;
+    static long recovered1;
+    static long critical1;
+    static long deaths1;
+    static double latitude1;
+    static double longitude1;
 
     public void getClient() throws IOException {
         Request request = new Request.Builder()
-                .url("https://covid-19-data.p.rapidapi.com/country?name=" + HomePageController.countryResult() )
+                .url("https://covid-19-data.p.rapidapi.com/country?name=" + HomePageController.countryResult())
                 .get()
                 .addHeader("x-rapidapi-host", "covid-19-data.p.rapidapi.com")
                 .addHeader("x-rapidapi-key", "889076f26amsh71f18b14ec7f68bp1b70acjsn0e8f34826e4e")
@@ -21,22 +32,29 @@ public class Rest {
 
         Response response = client.newCall(request).execute();
         response.close();
-        response1 = response.body().string();
+//        response1 = response.body().string();
 
-//        ObjectMapper mapper = new ObjectMapper();
-//        try {
-//            String resp = response.body().string();
-//            CountryDto cD = mapper.readValue(resp, CountryDto.class);
-//        }
-//        catch (JsonGenerationException e) {
-//            e.printStackTrace();
-//        } catch (JsonMappingException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String resp = response.body().string();
+            CountryDto cD = mapper.readValue(resp, CountryDto.class);
+            System.out.println(cD);
+            country1 = cD.getCountry();
+            confirmed1 = cD.getConfirmed();
+            recovered1 = cD.getRecovered();
+            critical1 = cD.getCritical();
+            deaths1 = cD.getDeaths();
+            latitude1 = cD.getLatitude();
+            longitude1 = cD.getLongitude();
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    static String getResponse1(){
-        return response1;
-    }
+//    static String getResponse1(){
+//        return response1;
+//    }
 }
--- src/main/java/Controllers/ResultPageController.java
+++ src/main/java/Controllers/ResultPageController.java
@@ -1,6 +1,5 @@
 package Controllers;
 
-import Dto.CountryDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mitchellbosecke.pebble.PebbleEngine;
 import com.mitchellbosecke.pebble.template.PebbleTemplate;
@@ -23,18 +22,24 @@ public class ResultPageController {
 
         ObjectMapper mapper = new ObjectMapper();
 
-        String resp = r.getResponse1();
-        CountryDto cD = mapper.readValue(resp, CountryDto.class);
+//        String resp = r.getResponse1();
+//        CountryDto cD = mapper.readValue(resp, CountryDto.class);
 
 //        CountryDto cD = new CountryDto();
         Map<String, Object> context = new HashMap<>();
         context.put("res", "res");
 
-        context.put("country", cD.getCountry());
-        context.put("confirmed", cD.getConfirmed());
-        context.put("recovered", cD.getRecovered());
-        context.put("critical", cD.getCritical());
-        context.put("deaths", cD.getDeaths());
+        context.put("country", r.country1);
+        context.put("confirmed", r.confirmed1);
+        context.put("recovered", r.recovered1);
+        context.put("critical", r.critical1);
+        context.put("deaths", r.deaths1);
+
+//        context.put("country", cD.getCountry());
+//        context.put("confirmed", cD.getConfirmed());
+//        context.put("recovered", cD.getRecovered());
+//        context.put("critical", cD.getCritical());
+//        context.put("deaths", cD.getDeaths());
 
         Writer writer = new StringWriter();
         compiledTemplate.evaluate(writer, context);
